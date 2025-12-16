e77663e1f3a857ad10ed4cf95da32a89ae5cc189
OutsydBaby <amichdot@gmail.com>
2025-12-16 15:47:52 +0100

    Added Car Images to the website

--- src/data/CarData.ts
+++ src/data/CarData.ts
@@ -5,35 +5,29 @@ export type Car = {
   category: string
 }
 
-
+import  Previa from '../image/Cars/Toyata Previa MPV Car.jpg'
+import Camry2005 from '../image/Cars/Toyota Camry 2005.jpg'
+import Camry2006 from '../image/Cars/Toyota Camry 2006.jpg'
+import Corrola2005 from '../image/Cars/Toyota Corrola 2005.jpg'
+import Corrola2008 from '../image/Cars/Toyota Corrola 2008.jpg'
+import Highlander2004 from '../image/Cars/Toyota Highlander  2004 SUV.jpg'
+import Highlander2003 from '../image/Cars/Toyota Highlander 2003 SUV.jpg'
+import Highlander2006 from '../image/Cars/Toyota Highlander 2006.jpg'
+import Highlander20062 from '../image/Cars/Toyota Highlander 2006 (2).jpg'
+import Highlander2007 from '../image/Cars/Toyota Highlander 2007.jpg'
+import Sienna2005 from '../image/Cars/Toyota Sienna 2005.jpg'
+import Sienna2006 from '../image/Cars/Toyota Sienna 2006 SUV.jpg'
 export const cars: Car[] = [
-  { id: 1, name: "Toyota Camry 2009", image: "https://source.unsplash.com/featured/?toyota+camry+2009", category: "Saloon" },
-  { id: 2, name: "Toyota Corolla 2006", image: "https://source.unsplash.com/featured/?toyota+corolla+2006", category: "Saloon" },
-  { id: 3, name: "Toyota Camry 1999", image: "https://source.unsplash.com/featured/?toyota+camry+1999", category: "Saloon" },
-  { id: 4, name: "Pontiac Vibe 2008", image: "https://source.unsplash.com/featured/?pontiac+vibe+2008", category: "Saloon" },
-  { id: 5, name: "Peugeot 406 1999", image: "https://source.unsplash.com/featured/?peugeot+406+1999", category: "Saloon" },
-  { id: 6, name: "Toyota Previa 2000 Space Bus", image: "https://source.unsplash.com/featured/?toyota+previa+2000+bus", category: "Bus" },
-  { id: 7, name: "Toyota Sienna 2002", image: "https://source.unsplash.com/featured/?toyota+sienna+2002", category: "Mini Bus" },
-  { id: 8, name: "Volkswagen Sharan 1997", image: "https://source.unsplash.com/featured/?volkswagen+sharan+1997", category: "Mini Bus" },
-  { id: 9, name: "Volkswagen Sharan 1997", image: "https://source.unsplash.com/featured/?volkswagen+sharan+1997+bus", category: "Mini Bus" },
-  { id: 10, name: "Mitsubishi 2002 Model", image: "https://source.unsplash.com/featured/?mitsubishi+2002", category: "Saloon" },
-
-  // Second page
-  { id: 11, name: "Toyota Highlander Black", image: "https://source.unsplash.com/featured/?toyota+highlander+black", category: "Jeep" },
-  { id: 12, name: "Toyota Highlander Custom", image: "https://source.unsplash.com/featured/?toyota+highlander", category: "Jeep" },
-  { id: 13, name: "Toyota Highlander Ash", image: "https://source.unsplash.com/featured/?toyota+highlander+ash", category: "Jeep" },
-  { id: 14, name: "Toyota Highlander Blue", image: "https://source.unsplash.com/featured/?toyota+highlander+blue", category: "Jeep" },
-  { id: 15, name: "Toyota Corolla Blue", image: "https://source.unsplash.com/featured/?toyota+corolla+blue", category: "Saloon" },
-  { id: 16, name: "Toyota Corolla Ash", image: "https://source.unsplash.com/featured/?toyota+corolla+ash", category: "Saloon" },
-  { id: 17, name: "Toyota Prius Silver", image: "https://source.unsplash.com/featured/?toyota+prius+silver", category: "Saloon" },
-  { id: 18, name: "Toyota Corolla Ash", image: "https://source.unsplash.com/featured/?toyota+corolla+ash", category: "Saloon" },
-  { id: 19, name: "Toyota Sienna Custom", image: "https://source.unsplash.com/featured/?toyota+sienna+custom", category: "Mini Bus" },
-  { id: 20, name: "Toyota Sienna Ash", image: "https://source.unsplash.com/featured/?toyota+sienna+ash", category: "Mini Bus" },
-  { id: 21, name: "Volkswagen Sharan Green", image: "https://source.unsplash.com/featured/?volkswagen+sharan+green", category: "Mini Bus" },
-  { id: 22, name: "Volkswagen Sharan Green", image: "https://source.unsplash.com/featured/?volkswagen+sharan+green+bus", category: "Mini Bus" },
-  { id: 23, name: "Volkswagen Sharan Green", image: "https://source.unsplash.com/featured/?volkswagen+sharan+green+van", category: "Mini Bus" },
-  { id: 24, name: "Volkswagen Sharan Red", image: "https://source.unsplash.com/featured/?volkswagen+sharan+red", category: "Mini Bus" },
-  { id: 25, name: "Volkswagen Sharan Red", image: "https://source.unsplash.com/featured/?volkswagen+sharan+red+bus", category: "Mini Bus" },
-  { id: 26, name: "Volkswagen Sharan Red", image: "https://source.unsplash.com/featured/?volkswagen+sharan+red+van", category: "Mini Bus" },
-  { id: 27, name: "Volkswagen Sharan Black", image: "https://source.unsplash.com/featured/?volkswagen+sharan+black", category: "Mini Bus" }
+  { id: 1, name: "Toyota Camry 2005", image: Camry2005, category: "Car" },
+  { id: 2, name: "Toyota Corolla 2008", image: Corrola2008, category: "Car" },
+  { id: 3, name: "Toyota Corrola 2005", image: Corrola2005, category: "Car" },
+  { id: 4, name: "Toyota Camry 2006", image: Camry2006, category: "Car" },
+  { id: 5, name: "Toyota Previa MPV", image: Previa, category: "Jeep" },
+  { id: 6, name: "Toyota Sienna 2005", image: Sienna2005, category: "SUV" },
+  { id: 7, name: "Toyota Sienna 2006", image: Sienna2006, category: "SUV" },
+  { id: 8, name: "Toyota Highlander 2004", image: Highlander2004, category: "SUV" },
+  { id: 9, name: "Toyota Highlander 2003", image: Highlander2003, category: "SUV" },
+  { id: 10, name: "Toyota Highlander 2006", image: Highlander2006, category: "Jeep" },
+  { id: 11, name: "Toyota Highlander 2006", image: Highlander20062, category: "Jeep" },
+  { id: 12, name: "Toyota Highlander 2007", image: Highlander2007, category: "Jeep" },
 ]
