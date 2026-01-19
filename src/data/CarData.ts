d7645e6f8d71e0ee656f12823bb6c1d3114a9ec2
AtomsHub <atomshubtechnologies@gmail.com>
2026-01-19 08:04:40 +0100

    Update CarData.ts

--- src/data/CarData.ts
+++ src/data/CarData.ts
@@ -17,6 +17,8 @@ import Highlander20062 from '../image/Cars/Toyota Highlander 2006 (2).jpg'
 import Highlander2007 from '../image/Cars/Toyota Highlander 2007.jpg'
 import Sienna2005 from '../image/Cars/Toyota Sienna 2005.jpg'
 import Sienna2006 from '../image/Cars/Toyota Sienna 2006 SUV.jpg'
+import Sienna from '../image/Cars/Toyota Sienna.jpg'
+import Hilux from '../image/Cars/Toyota Hilux.jpg'
 export const cars: Car[] = [
   { id: 1, name: "Toyota Camry 2005", image: Camry2005, category: "Car" },
   { id: 2, name: "Toyota Corolla 2008", image: Corrola2008, category: "Car" },
@@ -30,4 +32,6 @@ export const cars: Car[] = [
   { id: 10, name: "Toyota Highlander 2006", image: Highlander2006, category: "Jeep" },
   { id: 11, name: "Toyota Highlander 2006", image: Highlander20062, category: "Jeep" },
   { id: 12, name: "Toyota Highlander 2007", image: Highlander2007, category: "Jeep" },
+   { id: 13, name: "Toyota Sienna 2008", image: Sienna, category: "Jeep" },
+   { id: 14, name: "2012 Toyota Hilux", image: Hilux2012, category: "Jeep" },
 ]
